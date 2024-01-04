0e7526b4fa2b5dbd2a24b2caecbbed3a36a68fd3
oswald <lscarrol@buffalo.edu>
2024-01-04 04:00:51 +0000

    Testing

---
+++ src/tiktok/test.py
@@ -0,0 +1,6 @@
+import pyktok as pyk
+    
+# download a single TikTok video and one line of metadata to the file "test_data.csv"
+    
+pyk.save_tiktok('https://www.tiktok.com/@tiktok/video/7106594312292453675?is_copy_url=1&is_from_webapp=v1',True,video_data.csv)
+    
